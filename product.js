a2f9dbe3355190541d4a5d992842388814495c1f
EdoRaus <edo@dutchgreenhouses.com>
2025-03-24 17:27:23 +0100

    Add files via upload

---
+++ product.js
@@ -0,0 +1,7 @@
+const products = {
+    "1": { "id": 1, "title": "Product 1", "description": "Description 1", "price": 10, "image": "image1.jpg" },
+    "2": { "id": 2, "title": "Product 2", "description": "Description 2", "price": 20, "image": "image2.jpg" },
+    "3": { "id": 3, "title": "Product 3", "description": "Description 3", "price": 30, "image": "image3.jpg" }
+};
+
+module.exports = products;
